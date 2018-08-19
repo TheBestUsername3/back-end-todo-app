3a872b01864f29cc58efdffa0a1673b946136ca0
Yunran Chen <yunran2018@gmail.com>
2018-08-19 20:24:19 +0000

    todo new

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,6 +1,16 @@
 class TodoController < ApplicationController
+  def create
+  t = Todo.new
+  t.description = params['description']
+  t.pomodoro_estimate = params['pomodoro_estimate']
+  t.save
+  redirect_to "/todo/show/#{ t.id }"
+end
   def show
+  @todo = Todo.find_by_id(params[:id])
 end
   def index
   end
+  def new
+end
 end
